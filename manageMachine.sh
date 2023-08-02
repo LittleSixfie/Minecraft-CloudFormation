bb207013d6e88db99b6e94d38738900ce4d7fe08
LittleSixfie <jorge-padron99@hotmail.com>
2023-08-02 16:47:09 +0200

    Improvements

---
+++ manageMachine.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [[ $# -ne 4 ]]
+then
+    echo "Recuerda que se necesita, el AccessID, AccessKey, Idmaquina, start or stop"
+    exit -1
+fi
+
+if [[ $4 -eq "start" ]]; then
+    AWS_ACCESS_KEY_ID="$1" AWS_SECRET_ACCESS_KEY="$2" aws ec2 start-instances --region=eu-central-1 --instance-ids "$3"
+elif [[ $4 -eq "stop" ]]; then
+    AWS_ACCESS_KEY_ID="$1" AWS_SECRET_ACCESS_KEY="$2" aws ec2 stop-instances --region=eu-central-1 --instance-ids "$3" 
+else
+    echo "not valid 4 parameter, remember start or stop"
+fi
